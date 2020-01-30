e177f502ad0f74348a99c3dec302fb5dc3c20519
Kate Steib <kate.steib@gmail.com>
2020-01-30 10:16:42 -0600

    Assignment 4 - Part 2 complete. Added ability to add employers and skills.

--- queries.sql
+++ queries.sql
@@ -1,6 +1,24 @@
 ## Part 1: Test it with SQL
 
+--Shows each column's field, type, null, key, default, extra.
+
+SHOW FIELDS from job;
+
+describe job;
+
+--Shows only the information we want: column_name and data_type.
+
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_schema = 'techjobs' AND table_name  = 'job';
+
 ## Part 2: Test it with SQL
+
+--List the names of the employers in St. Louis city.
+
+SELECT name
+FROM employer
+WHERE location = "St. Louis";
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 250, message = "Description is limited to 250 maximum characters.")
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill() { }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
 }
